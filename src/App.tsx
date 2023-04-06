21c9f0b6e92142fc2faa731b6b183790ff534e65
Quentin Fortier <qpfortier@gmail.com>
2023-04-06 20:39:11 +0200

    Clean

--- src/App.tsx
+++ src/App.tsx
@@ -97,7 +97,7 @@ export default function App(props) {
                 >
                     <Toolbar>
                         <GithubCorner
-                            href="https://apprendre-sql.github.io"
+                            href="https://github.com/apprendre-sql/apprendre-sql.github.io"
                             direction="right"
                             size="60"
                         />
